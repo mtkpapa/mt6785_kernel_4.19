aa360b2fdba44db7b2b83037ef1ef63bc2169977
mtkpapa <ebanashka10028@gmail.com>
2025-09-02 16:56:51 +0300

    build.sh: Rework build script

--- build.sh
+++ build.sh
@@ -1,33 +1,26 @@
-TARGET_DEVICE=$1
 BUILD_DATE=$(date "+%Y%m%d-%H%M")
 
 #ccache
 export CCACHE_DIR="$HOME/.cache/ccache_mikernel"
-export CC="ccache gcc"
-export CXX="ccache g++"
-export PATH="/usr/lib/ccache:$PATH"
 echo "CCACHE_DIR: [$CCACHE_DIR]"
 
-MAKE_ARGS="O=out \
-CC=clang \
-AR=llvm-ar \
-NM=llvm-nm \
-OBJDUMP=llvm-objdump \
-STRIP=llvm-strip \
-HOSTCC=clang \
-HOSTCXX=clang++ \
-LD=ld.lld \
-CROSS_COMPILE=aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-LLVM=1 \
-LLVM_IAS=1"
+MAKE_ARGS=(
+    O=out
+    "CC=ccache clang"
+    "CXX=ccache clang++"
+    AR=llvm-ar
+    NM=llvm-nm
+    OBJDUMP=llvm-objdump
+    STRIP=llvm-strip
+    LD=ld.lld
+    CROSS_COMPILE=aarch64-linux-gnu-
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    LLVM=1
+    LLVM_IAS=1
+)
 
-if [ ! -f "arch/arm64/configs/${TARGET_DEVICE}_defconfig" ]; then
-    echo "No [${TARGET_DEVICE}] defconfig found."
-    echo "Avaliable defconfigs:"
-    ls arch/arm64/configs/*_defconfig
-    exit 1
-fi
+local_version_str="-perf"
+local_version_date_str="-z419-$(date +%Y%m%d)"
 
 KSU_ZIP_STR=noksu
 if [ "$2" == "ksu" ]; then
@@ -37,46 +30,45 @@ else
     KSU_E=0
 fi
 
+rm -rf out/
+rm -rf AnyKernel3/
+
 #setting up AK
 git clone https://github.com/mtkpapa/AnyKernel3 -b rosemary-4.19
 
 if [ $KSU_E -eq 1 ]; then
-    echo "dloading ksu & applying patches"
-    curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
-#    wget https://gist.githubusercontent.com/zainarbani/2b482e9e9c415a644953397b6ba5571f/raw/b66cf8d0683b5397fc1f7cc6b33ff12cc9bf9292/ksu.patch
-#    git apply ksu.patch
+    echo "Downloading KernelSU-Next"
+    curl -LSs "https://raw.githubusercontent.com/mtkpapa/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
 else 
-    echo "no ksu build"
+    echo "Building without KernelSU-Next"
 fi
 
-rm -rf out/
-
-echo "-ChoynikAmogusSon" > localversion
-
-#----------------------build shit here
+#----------------------build stuff here
 
 echo "======= START OF BUILD ======="
-make $MAKE_ARGS ${TARGET_DEVICE}_defconfig
+make "${MAKE_ARGS[@]}" rosemary_defconfig
+
+sed -i "s/${local_version_str}/${local_version_date_str}/g" out/.config
 
 if [ $KSU_E -eq 1 ]; then
     scripts/config --file out/.config -e KSU \
     -e KSU_SUSFS_HAS_MAGIC_MOUNT \
-    -e KSU_SUSFS_SUS_PATH \
+    -d KSU_SUSFS_SUS_PATH \
     -e KSU_SUSFS_SUS_MOUNT \
     -e KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT \
     -e KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT \
     -e KSU_SUSFS_SUS_KSTAT \
-    -e KSU_SUSFS_SUS_OVERLAYFS \
+    -d KSU_SUSFS_SUS_OVERLAYFS \
     -e KSU_SUSFS_TRY_UMOUNT \
     -e KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT \
     -e KSU_SUSFS_SPOOF_UNAME \
     -e KSU_SUSFS_ENABLE_LOG \
     -e KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS \
     -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
-    -e KSU_SUSFS_OPEN_REDIRECT \
-    -e KSU_SUSFS_SUS_SU
+    -d KSU_SUSFS_OPEN_REDIRECT \
+    -d KSU_SUSFS_SUS_SU
 else
-	scripts/config --file out/.config -d KSU \
+scripts/config --file out/.config -d KSU \
     -d KSU_SUSFS_HAS_MAGIC_MOUNT \
     -d KSU_SUSFS_SUS_PATH \
     -d KSU_SUSFS_SUS_MOUNT \
@@ -94,35 +86,31 @@ else
     -d KSU_SUSFS_SUS_SU
 fi
 
-make $MAKE_ARGS -j$(nproc --all)
+make "${MAKE_ARGS[@]}" -j$(nproc --all)
 
-echo "======= BUILDING DTBO ======="
 curl -s "https://android.googlesource.com/platform/system/libufdt/+/refs/heads/master/utils/src/mkdtboimg.py?format=TEXT" | base64 --decode > mkdtboimg.py
 python3 mkdtboimg.py create AnyKernel3/dtbnew out/arch/arm64/boot/dts/mediatek/mt6785.dtb
 
 echo "======= END OF BUILD ======="
 
-KOUT_PATH="/mnt/d/users/juan/kernels/${TARGET_DEVICE}/"
-ZIP_NAME="Choynik-$(date "+%Y%m%d-%H%M").zip"
+ZIP_NAME="OverHeat-Next-$(date "+%Y%m%d-%H%M").zip"
 
-
-if [ -f "out/arch/arm64/boot/Image.gz" ]; then
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
     echo "Image found. Build successful"
     cd AnyKernel3
-	cp ../out/arch/arm64/boot/Image.gz Image.gz
+	cp ../out/arch/arm64/boot/Image.gz-dtb Image.gz-dtb
 	zip -r9 ../$ZIP_NAME -- *
 	cd ..
-	cp $ZIP_NAME $KOUT_PATH
+	cp $ZIP_NAME ./
 	rm -rf $ZIP_NAME
 else
-    echo "Image not found. Pizdec blyat"
+    echo "Image not found. Build failed!"
     exit 1
 fi
 
 echo "======= CLEANING UP ======="
 
-rm -rf mkdtboimg.py
-rm -rf KernelSU-Next/
-rm -rf out/
-rm -rf localversion
-rm -rf AnyKernel3
+rm -rf mkdtboimg.py && echo "  RM      mkdtboimg.py"
+rm -rf KernelSU-Next/ && echo "  RM      KernelSU-Next"
+rm -rf out/ && echo "  RM      out"
+rm -rf AnyKernel3 && echo "  RM      AnyKernel3"
